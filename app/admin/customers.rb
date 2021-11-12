95d7d220d99acb2b20354665a24cf5eb62d66d4e
AnnHo01 <punri1304@gmail.com>
2021-11-12 00:49:11 -0600

    Add validation to model

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -5,7 +5,17 @@ ActiveAdmin.register Customer do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :fname, :tel, :email, :notes
+  permit_params :fname, :tel, :email, :notes, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
+
   #
   # or
   #
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
   has_one_attached :image
+  validate :fname, :tel, presence: true
+  validate :fname, uniqueness: true
 end
